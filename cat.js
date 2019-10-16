6f14f0007fd18a54b49bd1b64f429031952c32b5
pysyren <12520193@gm.uit.edu.vn>
2019-10-16 22:52:06 +0700

    Upgrade method eat for cat

--- cat.js
+++ cat.js
@@ -1,9 +1,16 @@
+var Mouse = require('./mouse');
+
 function Cat(){
     this.stomach = [];
 }
 
 Cat.prototype.eat = function(mouse){
-    this.stomach.push(mouse);
+    if(animal instanceof Mouse)
+    {
+        this.stomach.push(mouse);
+    }else{
+        throw new Error("Can't eat, please check and try again!!!");
+    }
 };
 
 module.exports = Cat;
